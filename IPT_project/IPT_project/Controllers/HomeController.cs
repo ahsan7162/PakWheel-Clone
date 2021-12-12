86bd8afe75f12a52df93af4b4e2d65127d0f46ca
Ahsan <imranahsan716@gmail.com>
2021-12-12 14:41:31 +0500

    cards updated, also added one filter, details page using database is also set

--- IPT_project/IPT_project/Controllers/HomeController.cs
+++ IPT_project/IPT_project/Controllers/HomeController.cs
@@ -10,57 +10,83 @@ using System.Configuration;
 using System.Data.SqlClient;
 using System.Net.Http;
 using System.Data;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
 
 namespace IPT_project.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string brand_name)
         {
+            if(brand_name == null)
+            {
+                //string Json = System.IO.File.ReadAllText("D:\\semester_7\\IPT\\IPT_project\\PythonScrapping\\data.json");
+                //JavaScriptSerializer ser = new JavaScriptSerializer();
+                //var carlist = ser.Deserialize<List<CarDetail.Rootobject>>(Json);
+                List<carCard> cars = new List<carCard>();
+                string connString = "Server = localhost;Database = master;Trusted_Connection=True";
 
-            //string Json = System.IO.File.ReadAllText("D:\\semester_7\\IPT\\IPT_project\\PythonScrapping\\data.json");
-            //JavaScriptSerializer ser = new JavaScriptSerializer();
-            //var carlist = ser.Deserialize<List<CarDetail.Rootobject>>(Json);
-            List<carCard> cars = new List<carCard>();
-            int i = 0;
-            string connString = "Server = localhost;Database = master;Trusted_Connection=True";
+                SqlConnection conn = new SqlConnection(connString);
+                Console.WriteLine("Openning Connection ...");
 
-            
-            //Console.WriteLine("Openning Connection ...");
-                using (SqlConnection conn = new SqlConnection(connString))
+                //open connection
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+
+                SqlCommand command;
+                SqlDataReader reader;
+                string sql;
+                sql = "select * from IPT_CourseProject.dbo.AdsData";
+                command = new SqlCommand(sql, conn);
+                reader = command.ExecuteReader();
+                while (reader.Read())
                 {
-                    i = 0;
-                    conn.Open();
+                    //Console.WriteLine(reader.GetValue(0)+" "+ reader.GetValue(1) + " " + reader.GetValue(2) + " " + reader.GetValue(3) + " " + reader.GetValue(4) + " " + reader.GetValue(5) + " " + reader.GetValue(6) + " " + reader.GetValue(7) + " " + reader.GetValue(8) + " " + reader.GetValue(9) + " " + reader.GetValue(10) + " " + reader.GetValue(11) + " " + reader.GetValue(12) + " ");
+                    cars.Add(new carCard { ad_id = reader.GetValue(0).ToString(), brand_name = reader.GetValue(1).ToString(), item_condition = reader.GetValue(2).ToString(), model_year = reader.GetValue(3).ToString(), manufacturer = reader.GetValue(4).ToString(), fuel_type = reader.GetValue(5).ToString(), transmission = reader.GetValue(6).ToString(), engine_capacity = reader.GetValue(7).ToString(), description = reader.GetValue(8).ToString(), engine_milegage = reader.GetValue(9).ToString(), image_url = reader.GetValue(11).ToString(), price = reader.GetValue(12).ToString() });
 
-                    //Console.WriteLine("Connection successful!");
-
-                    SqlCommand command;
-                    SqlDataReader reader;
-                    DataTable dt = new DataTable();
-                    string sql;
-                    sql = "select * from IPT_CourseProject.dbo.AdsData";
-                    using (command = new SqlCommand(sql, conn))
-                    {
-                        i = 0;
-                        using (reader = command.ExecuteReader())
-                        {
-                            dt.Load(reader);
-                        foreach (DataRow dr in dt.Rows)
-                        {
-                            object[] items = dr.ItemArray;
-                            if (items[0].ToString() != string.Empty)
-                            {
-                                cars[0].ad_id = items[0].ToString();
-                            }
-                        }
-                        
-                    }
-
-                    }
                 }
-            //Console.WriteLine(cars);
-            ViewBag.i = i;
-            return View();
+                //Console.WriteLine(cars);
+                return View(cars);
+            }
+            else
+            {
+                List<carCard> cars = new List<carCard>();
+                string connString = "Server = localhost;Database = master;Trusted_Connection=True";
+
+                SqlConnection conn = new SqlConnection(connString);
+                Console.WriteLine("Openning Connection ...");
+
+                //open connection
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+
+                SqlCommand command;
+                SqlDataReader reader;
+                string sql;
+                sql = "select * from IPT_CourseProject.dbo.AdsData";
+                if (brand_name != null)
+                {
+                    sql = sql + " where brand_name = @brand_name";
+                }
+                Console.WriteLine(sql);
+                command = new SqlCommand(sql, conn);
+                command.Parameters.AddWithValue("brand_name", brand_name);
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    //Console.WriteLine(reader.GetValue(0)+" "+ reader.GetValue(1) + " " + reader.GetValue(2) + " " + reader.GetValue(3) + " " + reader.GetValue(4) + " " + reader.GetValue(5) + " " + reader.GetValue(6) + " " + reader.GetValue(7) + " " + reader.GetValue(8) + " " + reader.GetValue(9) + " " + reader.GetValue(10) + " " + reader.GetValue(11) + " " + reader.GetValue(12) + " ");
+                    cars.Add(new carCard { ad_id = reader.GetValue(0).ToString(), brand_name = reader.GetValue(1).ToString(), item_condition = reader.GetValue(2).ToString(), model_year = reader.GetValue(3).ToString(), manufacturer = reader.GetValue(4).ToString(), fuel_type = reader.GetValue(5).ToString(), transmission = reader.GetValue(6).ToString(), engine_capacity = reader.GetValue(7).ToString(), description = reader.GetValue(8).ToString(), engine_milegage = reader.GetValue(9).ToString(), image_url = reader.GetValue(11).ToString(), price = reader.GetValue(12).ToString() });
+
+                }
+                //Console.WriteLine(cars);
+                return View(cars);
+            }
+
+           
         }
 
 
@@ -77,11 +103,78 @@ namespace IPT_project.Controllers
 
             return View();
         }
+        public static async Task<dynamic> Run(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            string responseString = await response.Content.ReadAsStringAsync();
+            dynamic jsonObject = JsonConvert.DeserializeObject(responseString);
+            return jsonObject;
+            Console.WriteLine($"{jsonObject["imageUrls"]}");
+            Console.WriteLine($"{jsonObject["carFeatures"]}");
+            Console.WriteLine($"{jsonObject["descriptionText"]}");
+
+
+        }
         public ActionResult Detail(string carID)
         {
-            ViewBag.id = carID;
+            if (carID != null)
+            {
+                ViewBag.id = carID;
 
-            return View();
+                carCard car = new carCard();
+                string connString = "Server = localhost;Database = master;Trusted_Connection=True";
+
+                SqlConnection conn = new SqlConnection(connString);
+                Console.WriteLine("Openning Connection ...");
+
+                //open connection
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+                SqlCommand command;
+                SqlDataReader reader;
+                string sql;
+                sql = "select * from IPT_CourseProject.dbo.AdsData where ad_id = @ad_id";
+                command = new SqlCommand(sql, conn);
+                command.Parameters.AddWithValue("ad_id", carID);
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    //car(new carC { ad_id = reader.GetValue(0).ToString(), brand_name = reader.GetValue(1).ToString(), item_condition = reader.GetValue(2).ToString(), model_year = reader.GetValue(3).ToString(), manufacturer = reader.GetValue(4).ToString(), fuel_type = reader.GetValue(5).ToString(), transmission = reader.GetValue(6).ToString(), engine_capacity = reader.GetValue(7).ToString(), description = reader.GetValue(8).ToString(), engine_milegage = reader.GetValue(9).ToString(), image_url = reader.GetValue(11).ToString(), price = reader.GetValue(12).ToString() });
+                    car.ad_id = reader.GetValue(0).ToString();
+                    car.brand_name = reader.GetValue(1).ToString();
+                    car.item_condition = reader.GetValue(2).ToString();
+                    car.model_year = reader.GetValue(3).ToString();
+                    car.manufacturer = reader.GetValue(4).ToString();
+                    car.fuel_type = reader.GetValue(5).ToString();
+                    car.transmission = reader.GetValue(6).ToString();
+                    car.engine_capacity = reader.GetValue(7).ToString();
+                    car.description = reader.GetValue(8).ToString();
+                    car.engine_milegage = reader.GetValue(9).ToString();
+                    car.image_url = reader.GetValue(11).ToString();
+                    car.price = reader.GetValue(12).ToString();
+                    break;
+                }
+
+
+
+
+
+                //string nameToSend = "https://www.pakwheels.com/used-cars/suzuki-mehran-1997-for-sale-in-toba-tek-singh-5701434";
+                //string baseURL = "https://pakwheelsaddetailsscrapper20211211160440.azurewebsites.net/api/ScrapDetails";
+                //string urlToInvoke = string.Format("{0}?adUrl={1}", baseURL, nameToSend);
+                //var jsonObject = Run(urlToInvoke);
+                //ViewBag.json = jsonObject["imageUrls"];
+                ViewBag.car = car;
+                return View();
+            }
+            else
+            {
+                ViewBag.car = null;
+                return View();
+            }
+            
         }
     }
 }
