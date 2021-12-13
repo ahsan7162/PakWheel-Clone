53f55e52f3296ad4bacb5bd1758e4196dcbcdac4
Muhammad Owais <owaisimran678@gmail.com>
2021-12-13 12:09:26 +0500

    Added Scrapper for live price updates of Toyota

---
+++ AzureFunctions/ToyotaWebsiteScrapper/ToyotaWebsiteScrapper/ToyotaCars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyotaWebsiteScrapper
+{
+    class ToyotaCars
+    {
+        public string url { get; set; }
+        public string price { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+        public string slogan { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine("====================== Car Information ======================");
+            Console.WriteLine($"** Name: {name} \n** Price: {price} \n** Slogan: {slogan} \n** Redirect Url: {url}\n** Image Url: {image}\n");
+        }
+    }
+}
